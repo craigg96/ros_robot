0ae2b5dd6bda7685ac8d4981a369ede01fc29c61
craigg96 <62603731+craigg96@users.noreply.github.com>
2020-04-27 16:24:23 +0100

    Update imu.ino

--- control/src/firmware/imu.ino
+++ control/src/firmware/imu.ino
@@ -1,3 +1,4 @@
+
 // Hardware setup:
 // MPU9250 Breakout --------- Arduino
 // VCC ---------------------- 3.3V
@@ -241,16 +242,10 @@ float eInt[3] = {0.0f, 0.0f, 0.0f};       // vector to hold integral error for M
 float magBias[3],magScale[3];
 float ypr_offset[3] = {0,0,0};
 
-// float acc[3] = {0.0,0.0,0.0};
-// float gyr[3] = {0.0,0.0,0.0};
-// float mgx[3] = {0.0,0.0,0.0};
-
 
 #include <ros.h>
-#include <imu_lib.h>
 #include <std_msgs/Float32.h>
 #include <std_msgs/UInt16.h>
-#include <std_msgs/Float32MultiArray.h>
 
 ros::NodeHandle  nh;
 
@@ -258,20 +253,11 @@ std_msgs::Float32 ros_roll;
 std_msgs::Float32 ros_pitch;
 std_msgs::Float32 ros_yaw;
 std_msgs::UInt16 ros_temp;
-std_msgs::Float32MultiArray ros_acc;
-std_msgs::Float32MultiArray ros_gyr;
-std_msgs::Float32MultiArray ros_mgn;
-
 
 ros::Publisher imu_roll("imu_roll", &ros_roll);
 ros::Publisher imu_pitch("imu_pitch", &ros_pitch);
 ros::Publisher imu_yaw("imu_yaw", &ros_yaw);
 ros::Publisher imu_temp("imu_temp", &ros_temp);
-ros::Publisher imu_acc("imu_acc", &ros_acc);
-ros::Publisher imu_gyr("imu_gyr", &ros_gyr);
-ros::Publisher imu_mgn("imu_mgn", &ros_mgn);
-
-
 
 void setup()
 {
@@ -281,10 +267,6 @@ void setup()
   nh.advertise(imu_pitch);
   nh.advertise(imu_yaw);
   nh.advertise(imu_temp);
-  nh.advertise(imu_acc);
-  nh.advertise(imu_gyr);
-  nh.advertise(imu_mgn);
-
 
   Wire.begin();
   
@@ -306,26 +288,15 @@ void setup()
 void loop()
 {  
 
-  float acc[3] = {ax,ay,az} ;
-  float gyr[3] = {gx,gy,gz} ;
-  float mgn[3] = {mx,my,mz} ;
-
-
   ros_roll.data = roll;
   ros_pitch.data = pitch;
   ros_yaw.data = yaw;
   ros_temp.data = temperature;
-  ros_acc.data = acc;
-  ros_gyr.data = gyr;
-  ros_mgn.data = mgn;
 
   imu_roll.publish( &ros_roll );
   imu_pitch.publish( &ros_pitch );
   imu_yaw.publish( &ros_yaw );
   imu_temp.publish( &ros_temp );
-  imu_acc.publish( &ros_acc );
-  imu_gyr.publish( &ros_gyr );
-  imu_mgn.publish( &ros_mgn );
 
   nh.spinOnce();
   
@@ -413,5 +384,317 @@ void loop()
     }
     }
 
+
+}
+
+#pragma once
+
+//===================================================================================================================
+//====== Set of useful function to access acceleration. gyroscope, magnetometer, and temperature data
+//===================================================================================================================
+
+void getMres()
+{
+    switch (Mscale)
+    {
+        // Possible magnetometer scales (and their register bit settings) are:
+        // 14 bit resolution (0) and 16 bit resolution (1)
+    case MFS_14BITS:
+        mRes = 10. * 4912. / 8190.; // Proper scale to return milliGauss
+        break;
+    case MFS_16BITS:
+        mRes = 10. * 4912. / 32760.0; // Proper scale to return milliGauss
+        break;
+    }
+}
+
+void getGres()
+{
+    switch (Gscale)
+    {
+        // Possible gyro scales (and their register bit settings) are:
+        // 250 DPS (00), 500 DPS (01), 1000 DPS (10), and 2000 DPS  (11). 
+              // Here's a bit of an algorith to calculate DPS/(ADC tick) based on that 2-bit value:
+    case GFS_250DPS:
+        gRes = 250.0 / 32768.0;
+        break;
+    case GFS_500DPS:
+        gRes = 500.0 / 32768.0;
+        break;
+    case GFS_1000DPS:
+        gRes = 1000.0 / 32768.0;
+        break;
+    case GFS_2000DPS:
+        gRes = 2000.0 / 32768.0;
+        break;
+    }
+}
+
+void getAres()
+{
+    switch (Ascale)
+    {
+        // Possible accelerometer scales (and their register bit settings) are:
+        // 2 Gs (00), 4 Gs (01), 8 Gs (10), and 16 Gs  (11). 
+              // Here's a bit of an algorith to calculate DPS/(ADC tick) based on that 2-bit value:
+    case AFS_2G:
+        aRes = 2.0 / 32768.0;
+        break;
+    case AFS_4G:
+        aRes = 4.0 / 32768.0;
+        break;
+    case AFS_8G:
+        aRes = 8.0 / 32768.0;
+        break;
+    case AFS_16G:
+        aRes = 16.0 / 32768.0;
+        break;
+    }
+}
+
+
+void readAccelData(int16_t* destination)
+{
+    uint8_t rawData[6];  // x/y/z accel register data stored here
+    readBytes(MPU9250_ADDRESS, ACCEL_XOUT_H, 6, &rawData[0]);  // Read the six raw data registers into data array
+    destination[0] = ((int16_t)rawData[0] << 8) | rawData[1];  // Turn the MSB and LSB into a signed 16-bit value
+    destination[1] = ((int16_t)rawData[2] << 8) | rawData[3];
+    destination[2] = ((int16_t)rawData[4] << 8) | rawData[5];
+}
+
+
+void readGyroData(int16_t* destination)
+{
+    uint8_t rawData[6];  // x/y/z gyro register data stored here
+    readBytes(MPU9250_ADDRESS, GYRO_XOUT_H, 6, &rawData[0]);  // Read the six raw data registers sequentially into data array
+    destination[0] = ((int16_t)rawData[0] << 8) | rawData[1];  // Turn the MSB and LSB into a signed 16-bit value
+    destination[1] = ((int16_t)rawData[2] << 8) | rawData[3];
+    destination[2] = ((int16_t)rawData[4] << 8) | rawData[5];
+}
+
+void readMagData(int16_t* destination)
+{
+    uint8_t rawData[7];  // x/y/z gyro register data, ST2 register stored here, must read ST2 at end of data acquisition
+    if (readByte(AK8963_ADDRESS, AK8963_ST1) & 0x01) { // wait for magnetometer data ready bit to be set
+        readBytes(AK8963_ADDRESS, AK8963_XOUT_L, 7, &rawData[0]);  // Read the six raw data and ST2 registers sequentially into data array
+        uint8_t c = rawData[6]; // End data read by reading ST2 register
+        if (!(c & 0x08)) { // Check if magnetic sensor overflow set, if not then report data
+            destination[0] = ((int16_t)rawData[1] << 8) | rawData[0];  // Turn the MSB and LSB into a signed 16-bit value
+            destination[1] = ((int16_t)rawData[3] << 8) | rawData[2];  // Data stored as little Endian
+            destination[2] = ((int16_t)rawData[5] << 8) | rawData[4];
+        }
+    }
+}
+
+int16_t readTempData()
+{
+    uint8_t rawData[2];  // x/y/z gyro register data stored here
+    readBytes(MPU9250_ADDRESS, TEMP_OUT_H, 2, &rawData[0]);  // Read the two raw data registers sequentially into data array 
+    return ((int16_t)rawData[0] << 8) | rawData[1];  // Turn the MSB and LSB into a 16-bit value
+}
+
+void initAK8963(float* destination)
+{
+    // First extract the factory calibration for each magnetometer axis
+    uint8_t rawData[3];  // x/y/z gyro calibration data stored here
+    writeByte(AK8963_ADDRESS, AK8963_CNTL, 0x00); // Power down magnetometer  
+    delay(10);
+    writeByte(AK8963_ADDRESS, AK8963_CNTL, 0x0F); // Enter Fuse ROM access mode
+    delay(10);
+    readBytes(AK8963_ADDRESS, AK8963_ASAX, 3, &rawData[0]);  // Read the x-, y-, and z-axis calibration values
+    destination[0] = (float)(rawData[0] - 128) / 256. + 1.;   // Return x-axis sensitivity adjustment values, etc.
+    destination[1] = (float)(rawData[1] - 128) / 256. + 1.;
+    destination[2] = (float)(rawData[2] - 128) / 256. + 1.;
+    writeByte(AK8963_ADDRESS, AK8963_CNTL, 0x00); // Power down magnetometer  
+    delay(10);
+    // Configure the magnetometer for continuous read and highest resolution
+    // set Mscale bit 4 to 1 (0) to enable 16 (14) bit resolution in CNTL register,
+    // and enable continuous mode data acquisition Mmode (bits [3:0]), 0010 for 8 Hz and 0110 for 100 Hz sample rates
+    writeByte(AK8963_ADDRESS, AK8963_CNTL, Mscale << 4 | Mmode); // Set magnetometer data resolution and sample ODR
+    delay(10);
+}
+
+
+void initMPU9250()
+{
+    // wake up device
+    writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x00); // Clear sleep mode bit (6), enable all sensors 
+    delay(100); // Wait for all registers to reset 
+
+  // get stable time source
+    writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x01);  // Auto select clock source to be PLL gyroscope reference if ready else
+    delay(200);
+
+    // Configure Gyro and Thermometer
+    // Disable FSYNC and set thermometer and gyro bandwidth to 41 and 42 Hz, respectively; 
+    // minimum delay time for this setting is 5.9 ms, which means sensor fusion update rates cannot
+    // be higher than 1 / 0.0059 = 170 Hz
+    // DLPF_CFG = bits 2:0 = 011; this limits the sample rate to 1000 Hz for both
+    // With the MPU9250, it is possible to get gyro sample rates of 32 kHz (!), 8 kHz, or 1 kHz
+    writeByte(MPU9250_ADDRESS, CONFIG, 0x03);
+
+    // Set sample rate = gyroscope output rate/(1 + SMPLRT_DIV)
+    writeByte(MPU9250_ADDRESS, SMPLRT_DIV, 0x04);  // Use a 200 Hz rate; a rate consistent with the filter update rate 
+                                      // determined inset in CONFIG above
+
+  // Set gyroscope full scale range
+  // Range selects FS_SEL and AFS_SEL are 0 - 3, so 2-bit values are left-shifted into positions 4:3
+    uint8_t c = readByte(MPU9250_ADDRESS, GYRO_CONFIG);
+    //  writeRegister(GYRO_CONFIG, c & ~0xE0); // Clear self-test bits [7:5] 
+    writeByte(MPU9250_ADDRESS, GYRO_CONFIG, c & ~0x02); // Clear Fchoice bits [1:0] 
+    writeByte(MPU9250_ADDRESS, GYRO_CONFIG, c & ~0x18); // Clear AFS bits [4:3]
+    writeByte(MPU9250_ADDRESS, GYRO_CONFIG, c | Gscale << 3); // Set full scale range for the gyro
+  // writeRegister(GYRO_CONFIG, c | 0x00); // Set Fchoice for the gyro to 11 by writing its inverse to bits 1:0 of GYRO_CONFIG
+
+  // Set accelerometer full-scale range configuration
+    c = readByte(MPU9250_ADDRESS, ACCEL_CONFIG);
+    //  writeRegister(ACCEL_CONFIG, c & ~0xE0); // Clear self-test bits [7:5] 
+    writeByte(MPU9250_ADDRESS, ACCEL_CONFIG, c & ~0x18); // Clear AFS bits [4:3]
+    writeByte(MPU9250_ADDRESS, ACCEL_CONFIG, c | Ascale << 3); // Set full scale range for the accelerometer 
+
+  // Set accelerometer sample rate configuration
+  // It is possible to get a 4 kHz sample rate from the accelerometer by choosing 1 for
+  // accel_fchoice_b bit [3]; in this case the bandwidth is 1.13 kHz
+    c = readByte(MPU9250_ADDRESS, ACCEL_CONFIG2);
+    writeByte(MPU9250_ADDRESS, ACCEL_CONFIG2, c & ~0x0F); // Clear accel_fchoice_b (bit 3) and A_DLPFG (bits [2:0])  
+    writeByte(MPU9250_ADDRESS, ACCEL_CONFIG2, c | 0x03); // Set accelerometer rate to 1 kHz and bandwidth to 41 Hz
+
+  // The accelerometer, gyro, and thermometer are set to 1 kHz sample rates, 
+  // but all these rates are further reduced by a factor of 5 to 200 Hz because of the SMPLRT_DIV setting
+
+    // Configure Interrupts and Bypass Enable
+    // Set interrupt pin active high, push-pull, hold interrupt pin level HIGH until interrupt cleared,
+    // clear on read of INT_STATUS, and enable I2C_BYPASS_EN so additional chips 
+    // can join the I2C bus and all can be controlled by the Arduino as master
+    writeByte(MPU9250_ADDRESS, INT_PIN_CFG, 0x22);
+    writeByte(MPU9250_ADDRESS, INT_ENABLE, 0x01);  // Enable data ready (bit 0) interrupt
+    delay(100);
+}
+
+
+
+// Wire.h read and write protocols
+void writeByte(uint8_t address, uint8_t subAddress, uint8_t data)
+{
+    Wire.beginTransmission(address);  // Initialize the Tx buffer
+    Wire.write(subAddress);           // Put slave register address in Tx buffer
+    Wire.write(data);                 // Put data in Tx buffer
+    Wire.endTransmission();           // Send the Tx buffer
+}
+
+uint8_t readByte(uint8_t address, uint8_t subAddress)
+{
+    uint8_t data; // `data` will store the register data   
+    Wire.beginTransmission(address);         // Initialize the Tx buffer
+    Wire.write(subAddress);                  // Put slave register address in Tx buffer
+    Wire.endTransmission(false);             // Send the Tx buffer, but send a restart to keep connection alive
+    Wire.requestFrom(address, (uint8_t)1);  // Read one byte from slave register address 
+    data = Wire.read();                      // Fill Rx buffer with result
+    return data;                             // Return data read from slave register
+}
+
+void readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t* dest)
+{
+    Wire.beginTransmission(address);   // Initialize the Tx buffer
+    Wire.write(subAddress);            // Put slave register address in Tx buffer
+    Wire.endTransmission(false);       // Send the Tx buffer, but send a restart to keep connection alive
+    uint8_t i = 0;
+    Wire.requestFrom(address, count);  // Read bytes from slave register address 
+    while (Wire.available()) {
+        dest[i++] = Wire.read();
+    }         // Put read results in the Rx buffer
+}
+
+
+
+void MadgwickQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz)
+{
+    float q1 = q[0], q2 = q[1], q3 = q[2], q4 = q[3];   // short name local variable for readability
+    float norm;
+    float hx, hy, _2bx, _2bz;
+    float s1, s2, s3, s4;
+    float qDot1, qDot2, qDot3, qDot4;
+
+    // Auxiliary variables to avoid repeated arithmetic
+    float _2q1mx;
+    float _2q1my;
+    float _2q1mz;
+    float _2q2mx;
+    float _4bx;
+    float _4bz;
+    float _2q1 = 2.0f * q1;
+    float _2q2 = 2.0f * q2;
+    float _2q3 = 2.0f * q3;
+    float _2q4 = 2.0f * q4;
+    float _2q1q3 = 2.0f * q1 * q3;
+    float _2q3q4 = 2.0f * q3 * q4;
+    float q1q1 = q1 * q1;
+    float q1q2 = q1 * q2;
+    float q1q3 = q1 * q3;
+    float q1q4 = q1 * q4;
+    float q2q2 = q2 * q2;
+    float q2q3 = q2 * q3;
+    float q2q4 = q2 * q4;
+    float q3q3 = q3 * q3;
+    float q3q4 = q3 * q4;
+    float q4q4 = q4 * q4;
+
+    // Normalise accelerometer measurement
+    norm = sqrt(ax * ax + ay * ay + az * az);
+    if (norm == 0.0f) return; // handle NaN
+    norm = 1.0f / norm;
+    ax *= norm;
+    ay *= norm;
+    az *= norm;
+
+    // Normalise magnetometer measurement
+    norm = sqrt(mx * mx + my * my + mz * mz);
+    if (norm == 0.0f) return; // handle NaN
+    norm = 1.0f / norm;
+    mx *= norm;
+    my *= norm;
+    mz *= norm;
+
+    // Reference direction of Earth's magnetic field
+    _2q1mx = 2.0f * q1 * mx;
+    _2q1my = 2.0f * q1 * my;
+    _2q1mz = 2.0f * q1 * mz;
+    _2q2mx = 2.0f * q2 * mx;
+    hx = mx * q1q1 - _2q1my * q4 + _2q1mz * q3 + mx * q2q2 + _2q2 * my * q3 + _2q2 * mz * q4 - mx * q3q3 - mx * q4q4;
+    hy = _2q1mx * q4 + my * q1q1 - _2q1mz * q2 + _2q2mx * q3 - my * q2q2 + my * q3q3 + _2q3 * mz * q4 - my * q4q4;
+    _2bx = sqrt(hx * hx + hy * hy);
+    _2bz = -_2q1mx * q3 + _2q1my * q2 + mz * q1q1 + _2q2mx * q4 - mz * q2q2 + _2q3 * my * q4 - mz * q3q3 + mz * q4q4;
+    _4bx = 2.0f * _2bx;
+    _4bz = 2.0f * _2bz;
+
+    // Gradient decent algorithm corrective step
+    s1 = -_2q3 * (2.0f * q2q4 - _2q1q3 - ax) + _2q2 * (2.0f * q1q2 + _2q3q4 - ay) - _2bz * q3 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q4 + _2bz * q2) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q3 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
+    s2 = _2q4 * (2.0f * q2q4 - _2q1q3 - ax) + _2q1 * (2.0f * q1q2 + _2q3q4 - ay) - 4.0f * q2 * (1.0f - 2.0f * q2q2 - 2.0f * q3q3 - az) + _2bz * q4 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q3 + _2bz * q1) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q4 - _4bz * q2) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
+    s3 = -_2q1 * (2.0f * q2q4 - _2q1q3 - ax) + _2q4 * (2.0f * q1q2 + _2q3q4 - ay) - 4.0f * q3 * (1.0f - 2.0f * q2q2 - 2.0f * q3q3 - az) + (-_4bx * q3 - _2bz * q1) * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q2 + _2bz * q4) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q1 - _4bz * q3) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
+    s4 = _2q2 * (2.0f * q2q4 - _2q1q3 - ax) + _2q3 * (2.0f * q1q2 + _2q3q4 - ay) + (-_4bx * q4 + _2bz * q2) * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q1 + _2bz * q3) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q2 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
+    norm = sqrt(s1 * s1 + s2 * s2 + s3 * s3 + s4 * s4);    // normalise step magnitude
+    norm = 1.0f / norm;
+    s1 *= norm;
+    s2 *= norm;
+    s3 *= norm;
+    s4 *= norm;
+
+    // Compute rate of change of quaternion
+    qDot1 = 0.5f * (-q2 * gx - q3 * gy - q4 * gz) - beta * s1;
+    qDot2 = 0.5f * (q1 * gx + q3 * gz - q4 * gy) - beta * s2;
+    qDot3 = 0.5f * (q1 * gy - q2 * gz + q4 * gx) - beta * s3;
+    qDot4 = 0.5f * (q1 * gz + q2 * gy - q3 * gx) - beta * s4;
+
+    // Integrate to yield quaternion
+    q1 += qDot1 * deltat;
+    q2 += qDot2 * deltat;
+    q3 += qDot3 * deltat;
+    q4 += qDot4 * deltat;
+    norm = sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);    // normalise quaternion
+    norm = 1.0f / norm;
+    q[0] = q1 * norm;
+    q[1] = q2 * norm;
+    q[2] = q3 * norm;
+    q[3] = q4 * norm;
 
 }
